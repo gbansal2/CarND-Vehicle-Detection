307cdf19167c73b14446c3af655dce8356b2091a
Gaurav Bansal <gaurav2.bansal@intel.com>
2017-08-22 18:11:19 -0700

    basic pipeline done for an image

---
+++ train_model.py
@@ -0,0 +1,105 @@
+import matplotlib.image as mpimg
+import matplotlib.pyplot as plt
+import numpy as np
+import cv2
+import glob
+import time
+import pickle
+from sklearn.svm import LinearSVC
+from sklearn.preprocessing import StandardScaler
+from skimage.feature import hog
+from sklearn.externals import joblib
+from helper_functions import *
+# NOTE: the next import is only valid for scikit-learn version <= 0.17
+# for scikit-learn >= 0.18 use:
+from sklearn.model_selection import train_test_split
+#from sklearn.cross_validation import train_test_split
+
+# Read in cars and notcars
+cars = []
+notcars = []
+cars = glob.glob('vehicles/*/*.png')
+notcars = glob.glob('non-vehicles/*/*.png')
+#for image in vehicle_images:
+#    cars.append(mpimg.imread(image))
+#for image in nonvehicle_images:
+#    notcars.append(mpimg.imread(image))
+##for image in images:
+#    if 'image' in image or 'extra' in image:
+#        notcars.append(image)
+#    else:
+#        cars.append(image)
+#
+# Reduce the sample size because
+# The quiz evaluator times out after 13s of CPU time
+#print("Number of Car images = ", len(cars))
+#print("Number of Not Car images = ", len(notcars))
+print("Shape of Car images = ", mpimg.imread(cars[0]).shape)
+print("Shape of Not Car images = ", mpimg.imread(notcars[0]).shape)
+#sample_size = 500
+#cars = cars[0:sample_size]
+#notcars = notcars[0:sample_size]
+
+### TODO: Tweak these parameters and see how the results change.
+color_space = 'HSV' # Can be RGB, HSV, LUV, HLS, YUV, YCrCb
+orient = 9  # HOG orientations
+pix_per_cell = 8 # HOG pixels per cell
+cell_per_block = 2 # HOG cells per block
+hog_channel = "ALL" # Can be 0, 1, 2, or "ALL"
+spatial_size = (32, 32) # Spatial binning dimensions
+hist_bins = 32    # Number of histogram bins
+spatial_feat = True # Spatial features on or off
+hist_feat = True # Histogram features on or off
+hog_feat = True # HOG features on or off
+y_start_stop = [None, None] # Min and max in y to search in slide_window()
+
+
+car_features = extract_features(cars, color_space=color_space, 
+                        spatial_size=spatial_size, hist_bins=hist_bins, 
+                        orient=orient, pix_per_cell=pix_per_cell, 
+                        cell_per_block=cell_per_block, 
+                        hog_channel=hog_channel, spatial_feat=spatial_feat, 
+                        hist_feat=hist_feat, hog_feat=hog_feat)
+notcar_features = extract_features(notcars, color_space=color_space, 
+                        spatial_size=spatial_size, hist_bins=hist_bins, 
+                        orient=orient, pix_per_cell=pix_per_cell, 
+                        cell_per_block=cell_per_block, 
+                        hog_channel=hog_channel, spatial_feat=spatial_feat, 
+                        hist_feat=hist_feat, hog_feat=hog_feat)
+
+X = np.vstack((car_features, notcar_features)).astype(np.float64)                        
+# Fit a per-column scaler
+X_scaler = StandardScaler().fit(X)
+# Apply the scaler to X
+scaled_X = X_scaler.transform(X)
+
+print('X shape = ', X.shape)
+
+with open('params.pkl', 'wb') as f: 
+    pickle.dump([orient, pix_per_cell, cell_per_block, spatial_size, hist_bins, X_scaler], f)
+
+# Define the labels vector
+y = np.hstack((np.ones(len(car_features)), np.zeros(len(notcar_features))))
+
+
+# Split up data into randomized training and test sets
+rand_state = np.random.randint(0, 100)
+X_train, X_test, y_train, y_test = train_test_split(
+    scaled_X, y, test_size=0.2, random_state=rand_state)
+
+print('Training data set size = ', len(y_train))
+print('Test data set size = ', len(y_test))
+
+print('Using:',orient,'orientations',pix_per_cell,
+    'pixels per cell and', cell_per_block,'cells per block')
+print('Feature vector length:', len(X_train[0]))
+# Use a linear SVC 
+svc = LinearSVC()
+# Check the training time for the SVC
+t=time.time()
+svc.fit(X_train, y_train)
+t2 = time.time()
+print(round(t2-t, 2), 'Seconds to train SVC...')
+# Check the score of the SVC
+print('Test Accuracy of SVC = ', round(svc.score(X_test, y_test), 4))
+joblib.dump(svc, 'svc_model.pkl') 
